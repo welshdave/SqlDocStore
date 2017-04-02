eeb1214eef172f638686a03c8913dd68ed9a1a23
Dave Lewis <dave@dllewis.org>
2017-04-02 20:19:06 +0100

    Index on Id

--- src/SqlDocStore.MsSql/Sql/CreateTable.sql
+++ src/SqlDocStore.MsSql/Sql/CreateTable.sql
@@ -1,3 +1,7 @@
 ﻿CREATE TABLE {@schema_name}.{@table_name} (
-	Document NVARCHAR(MAX)
+	Document NVARCHAR(MAX),
+	vId AS JSON_VALUE(Document,'$.Id')
 )
+
+CREATE INDEX idx_{@table_name}_json_Id
+ON {@schema_name}.{@table_name}(vId)
