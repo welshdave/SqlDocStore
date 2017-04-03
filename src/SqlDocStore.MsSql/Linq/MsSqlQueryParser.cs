48482016cc6a5bfd977ea650bb7cdda10550ce26
Dave Lewis <dave@dllewis.org>
2017-04-03 12:51:54 +0100

    Moving things around for querying work

---
+++ src/SqlDocStore.MsSql/Linq/MsSqlQueryParser.cs
@@ -0,0 +1,32 @@
+﻿namespace SqlDocStore.MsSql.Linq
+{
+    using System;
+    using System.Linq.Expressions;
+    using Remotion.Linq;
+    using Remotion.Linq.Parsing.ExpressionVisitors.Transformation;
+    using Remotion.Linq.Parsing.Structure;
+    using Remotion.Linq.Parsing.Structure.NodeTypeProviders;
+
+    public class MsSqlQueryParser : IQueryParser
+    {
+        private readonly QueryParser _parser;
+
+        public MsSqlQueryParser(Action<MethodInfoBasedNodeTypeRegistry> registerNodeTypes = null)
+        {
+            var transformerRegistry = ExpressionTransformerRegistry.CreateDefault();
+            
+            var processor = ExpressionTreeParser.CreateDefaultProcessor(transformerRegistry);
+
+            var nodeTypeRegistry = MethodInfoBasedNodeTypeRegistry.CreateFromRelinqAssembly();
+            registerNodeTypes?.Invoke(nodeTypeRegistry);
+
+            var expressionTreeParser =
+                new ExpressionTreeParser(nodeTypeRegistry, processor);
+            _parser = new QueryParser(expressionTreeParser);
+        }
+        public QueryModel GetParsedQuery(Expression expressionTreeRoot)
+        {
+            return _parser.GetParsedQuery(expressionTreeRoot);
+        }
+    }
+}
--- src/SqlDocStore.MsSql/MsSqlDocumentSession.cs
+++ src/SqlDocStore.MsSql/MsSqlDocumentSession.cs
@@ -3,10 +3,10 @@
     using System;
     using System.Collections.Generic;
     using System.Data.SqlClient;
-    using System.Deployment.Internal;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+    using Linq;
     using Sql;
 
     public class MsSqlDocumentSession : DocumentSessionBase
