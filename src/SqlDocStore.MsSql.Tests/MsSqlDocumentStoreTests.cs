7786f6369afc0536686bc555095602586e495589
Dave Lewis <dave@dllewis.org>
2018-03-28 21:07:23 +0100

    Added schema tests back in

--- src/SqlDocStore.MsSql.Tests/MsSqlDocumentStoreTests.cs
+++ src/SqlDocStore.MsSql.Tests/MsSqlDocumentStoreTests.cs
@@ -44,107 +44,108 @@
             }
         }
 
-    //TODO: Rewrite below so can do schema tests.
-    //    [Fact]
-    //    public async Task should_initialize_custom_schema_when_required()
-    //    {
-    //        const string schema = "newschema";
-    //        const string table = "Test";
-    //        using (var fixture = GetFixture())
-    //        {
-    //            var store = new MsSqlDocumentStore(new DocumentStoreSettings(fixture.ConnectionString)
-    //            {
-    //                Schema = schema,
-    //                Table = table
-    //            });
+#if !NETCOREAPP1_0
+        [Fact]
+        public async Task should_initialize_custom_schema_when_required()
+        {
+            const string schema = "newschema";
+            const string table = "Test";
+            using (var fixture = GetFixture())
+            {
+                var store = new MsSqlDocumentStore(new DocumentStoreSettings(fixture.ConnectionString)
+                {
+                    Schema = schema,
+                    Table = table
+                });
 
-    //            await fixture.CreateDatabase();
+                await fixture.CreateDatabase();
 
-    //            await store.InitializeSchema();
+                await store.InitializeSchema();
 
-    //            using (var connection = new SqlConnection(fixture.ConnectionString))
-    //            {
-    //                await connection.OpenAsync().ConfigureAwait(false);
-    //                var schemas = connection.GetSchema(Tables);
-    //                schemas.Rows.Count.ShouldBe(1);
-    //                schemas.Rows[0]["TABLE_SCHEMA"].ShouldBe(schema);
-    //                schemas.Rows[0]["TABLE_NAME"].ShouldBe(table);
-    //            }
-    //        }
-    //    }
+                using (var connection = new SqlConnection(fixture.ConnectionString))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    var schemas = connection.GetSchema(Tables);
+                    schemas.Rows.Count.ShouldBe(1);
+                    schemas.Rows[0]["TABLE_SCHEMA"].ShouldBe(schema);
+                    schemas.Rows[0]["TABLE_NAME"].ShouldBe(table);
+                }
+            }
+        }
 
-    //    [Fact]
-    //    public async Task should_initialize_default_schema_when_required()
-    //    {
-    //        using (var fixture = GetFixture())
-    //        {
-    //            var settings = new DocumentStoreSettings(fixture.ConnectionString);
-    //            var store = new MsSqlDocumentStore(settings);
+        [Fact]
+        public async Task should_initialize_default_schema_when_required()
+        {
+            using (var fixture = GetFixture())
+            {
+                var settings = new DocumentStoreSettings(fixture.ConnectionString);
+                var store = new MsSqlDocumentStore(settings);
 
-    //            await fixture.CreateDatabase();
+                await fixture.CreateDatabase();
 
-    //            await store.InitializeSchema();
+                await store.InitializeSchema();
 
-    //            using (var connection = new SqlConnection(fixture.ConnectionString))
-    //            {
-    //                await connection.OpenAsync().ConfigureAwait(false);
-    //                var schemas = connection.GetSchema(Tables);
-    //                schemas.Rows.Count.ShouldBe(1);
-    //                schemas.Rows[0]["TABLE_SCHEMA"].ShouldBe(settings.Schema);
-    //                schemas.Rows[0]["TABLE_NAME"].ShouldBe(settings.Table);
-    //            }
-    //        }
-    //    }
+                using (var connection = new SqlConnection(fixture.ConnectionString))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    var schemas = connection.GetSchema(Tables);
+                    schemas.Rows.Count.ShouldBe(1);
+                    schemas.Rows[0]["TABLE_SCHEMA"].ShouldBe(settings.Schema);
+                    schemas.Rows[0]["TABLE_NAME"].ShouldBe(settings.Table);
+                }
+            }
+        }
 
-    //    [Fact]
-    //    public async Task should_not_initialize_custom_schema_when_schemacreation_none()
-    //    {
-    //        const string schema = "different";
-    //        const string table = "MyDocs";
+        [Fact]
+        public async Task should_not_initialize_custom_schema_when_schemacreation_none()
+        {
+            const string schema = "different";
+            const string table = "MyDocs";
 
-    //        using (var fixture = GetFixture())
-    //        {
-    //            var settings = new DocumentStoreSettings(fixture.ConnectionString)
-    //            {
-    //                SchemaCreation = SchemaCreation.None,
-    //                Schema = schema,
-    //                Table = table
-    //            };
-    //            var store = new MsSqlDocumentStore(settings);
+            using (var fixture = GetFixture())
+            {
+                var settings = new DocumentStoreSettings(fixture.ConnectionString)
+                {
+                    SchemaCreation = SchemaCreation.None,
+                    Schema = schema,
+                    Table = table
+                };
+                var store = new MsSqlDocumentStore(settings);
 
-    //            await fixture.CreateDatabase();
+                await fixture.CreateDatabase();
 
-    //            await store.InitializeSchema();
+                await store.InitializeSchema();
 
-    //            using (var connection = new SqlConnection(fixture.ConnectionString))
-    //            {
-    //                await connection.OpenAsync().ConfigureAwait(false);
-    //                var schemas = connection.GetSchema(Tables);
-    //                schemas.Rows.Count.ShouldBe(0);
-    //            }
-    //        }
-    //    }
+                using (var connection = new SqlConnection(fixture.ConnectionString))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    var schemas = connection.GetSchema(Tables);
+                    schemas.Rows.Count.ShouldBe(0);
+                }
+            }
+        }
 
-    //    [Fact]
-    //    public async Task should_not_initialize_default_schema_when_schemacreation_none()
-    //    {
-    //        using (var fixture = GetFixture())
-    //        {
-    //            var settings =
-    //                new DocumentStoreSettings(fixture.ConnectionString) {SchemaCreation = SchemaCreation.None};
-    //            var store = new MsSqlDocumentStore(settings);
+        [Fact]
+        public async Task should_not_initialize_default_schema_when_schemacreation_none()
+        {
+            using (var fixture = GetFixture())
+            {
+                var settings =
+                    new DocumentStoreSettings(fixture.ConnectionString) { SchemaCreation = SchemaCreation.None };
+                var store = new MsSqlDocumentStore(settings);
 
-    //            await fixture.CreateDatabase();
+                await fixture.CreateDatabase();
 
-    //            await store.InitializeSchema();
+                await store.InitializeSchema();
 
-    //            using (var connection = new SqlConnection(fixture.ConnectionString))
-    //            {
-    //                await connection.OpenAsync().ConfigureAwait(false);
-    //                var schemas = connection.GetSchema(Tables);
-    //                schemas.Rows.Count.ShouldBe(0);
-    //            }
-    //        }
-    //    }
+                using (var connection = new SqlConnection(fixture.ConnectionString))
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    var schemas = connection.GetSchema(Tables);
+                    schemas.Rows.Count.ShouldBe(0);
+                }
+            }
+        }
+#endif
     }
 }
